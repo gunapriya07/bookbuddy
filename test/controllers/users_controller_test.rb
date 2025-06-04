95438e7070ddb20549ba8300331ee09d08ea0b19
gunapriya07 <gunapriya77776@gmail.com>
2025-06-04 14:11:13 +0530

    added the scaffold_user with model and views

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,13 +15,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create user" do
-    assert_difference("User.count") do
-      post users_url, params: { user: { email: @user.email, name: @user.name, password_digest: @user.password_digest } }
-    end
-
-    assert_redirected_to user_url(User.last)
+ test "should create user" do
+  assert_difference("User.count") do
+    post users_url, params: {
+      user: {
+        name: "Test User",
+        email: "unique_user@example.com",  # Must be unique
+        password_digest: "securepassword123"
+      }
+    }
   end
+
+  assert_redirected_to user_url(User.last)
+end
+
 
   test "should show user" do
     get user_url(@user)
@@ -33,10 +40,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, name: @user.name, password_digest: @user.password_digest } }
-    assert_redirected_to user_url(@user)
-  end
+test "should update user" do
+  patch user_url(@user), params: {
+    user: {
+      name: "Updated Name",
+      email: "updated_email@example.com",  # Make sure it's different if your test db enforces uniqueness
+      password_digest: "newsecurepassword"
+    }
+  }
+
+  assert_redirected_to user_url(@user)
+  @user.reload
+  assert_equal "Updated Name", @user.name
+end
+
 
   test "should destroy user" do
     assert_difference("User.count", -1) do
