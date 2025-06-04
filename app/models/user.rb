eaa34386e09867d0053d598644b82fc6ec583164
gunapriya07 <gunapriya77776@gmail.com>
2025-06-04 13:49:55 +0530

    added the scaffold_user with model and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password_digest, presence: true
 end
