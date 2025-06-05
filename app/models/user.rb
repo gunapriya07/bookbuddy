4a7302debedd730ac159f319a9e86370f5af6882
gunapriya07 <gunapriya77776@gmail.com>
2025-06-05 06:57:08 +0530

    added user_authentication_signup&login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
