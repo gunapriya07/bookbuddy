3d419e49a78bb39915bf3425f0d5dc2f2bba7bf5
gunapriya07 <gunapriya77776@gmail.com>
2025-06-05 07:14:08 +0530

    added user_authentication

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,18 +1,37 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
+  def setup
+    @user = User.create!(
+      name: "Test User",
+      email: "test@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+  end
+
+  test "should get login form" do
+    get login_url
     assert_response :success
   end
 
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
+  test "should log in with valid credentials" do
+    post login_url, params: { email: @user.email, password: "password" }
+    assert_redirected_to root_url
+    assert_equal @user.id, session[:user_id]
   end
 
-  test "should get destroy" do
-    get sessions_destroy_url
+  test "should not log in with invalid credentials" do
+    post login_url, params: { email: @user.email, password: "wrong" }
     assert_response :success
+    assert_nil session[:user_id]
+    assert_select "div.alert", /Invalid email or password/
+  end
+
+  test "should log out successfully" do
+    post login_url, params: { email: @user.email, password: "password" }
+    delete logout_url
+    assert_redirected_to login_url
+    assert_nil session[:user_id]
   end
 end
