0b5e1e2ac412ee019e732479320eb201ae16ab15
gunapriya07 <gunapriya77776@gmail.com>
2025-06-07 08:32:26 +0530

    added Authorization_UTfILTERING

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
+  get "signup", to: "users#new" # <-- ✅ This line is required
   # resources :users
   root "users#index"
   get "up" => "rails/health#show", as: :rails_health_check
