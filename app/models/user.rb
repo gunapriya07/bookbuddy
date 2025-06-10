bc526df0bd46fef9455a1056795918289b4395cd
gunapriya07 <gunapriya77776@gmail.com>
2025-06-10 09:19:02 +0530

    added validations & flashMessages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
